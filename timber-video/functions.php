e080a5e5c15ad258e2fa509935e8d1d7569076ba
Alex Y <alx.young234@gmail.com>
2021-04-01 19:57:30 -0600

    Add files via upload

---
+++ timber-video/functions.php
@@ -0,0 +1,3 @@
+<?php
+
+$timber = new Timber\Timber();
---
+++ timber-video/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$context = Timber::context();
+$context['posts'] = new Timber\PostQuery();
+
+$templates = array("index.twig");
+Timber::render($templates, $context);
